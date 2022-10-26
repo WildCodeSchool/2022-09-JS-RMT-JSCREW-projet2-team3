82caa3ea7ad832c4132f820c473c8d9de58a7a13
charliepian <charlie.piancatelli@gmail.com>
2022-10-26 16:22:29 +0200

    Création de la mise en page Navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,29 @@
 function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <div className="container-fluid ">
+        <button className="navbar-brand" type="button">
+          <img
+            src="#"
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+          StreamWood
+        </button>
+      </div>
+      <ul>
+        <li>Film</li>
+        <li>Mon compte</li>
+        <li>Nos valeurs</li>
+      </ul>
+      <form>
+        <input type="text" />
+        <button type="button">BTN</button>
+      </form>
+    </nav>
+  );
 }
 
 export default NavBar;
