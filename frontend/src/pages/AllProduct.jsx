89e75aa76d2763b5be09d8434db41bbad608147c
arouxel17 <alexandrerouxel17@gmail.com>
2022-10-26 17:13:49 +0200

    ajout 1ere image

--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,5 +1,15 @@
 function AllProduct() {
-  return <div>AllProduct</div>;
+  return (
+    <div className="allProducts">
+      <h2>Tous les films</h2>
+      <h3>Catégories</h3>
+      <img
+        className="col-md-1 img-fluid"
+        src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
+        alt="1er film action"
+      />
+    </div>
+  );
 }
 
 export default AllProduct;
