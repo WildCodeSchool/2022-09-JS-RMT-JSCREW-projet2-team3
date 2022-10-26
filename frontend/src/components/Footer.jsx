a02567b409bb3b2b93f7bf0fc95d0c43075d523e
serpentaire <glemoine@hotmail.fr>
2022-10-26 16:39:35 +0200

    mise en place structure

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,54 @@
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <input className="name" type="text" name="name" />
+      <input
+        className="adresseEmail"
+        type="text"
+        adresseEmail="Adresse Email"
+      />
+      <textarea className="message">Tapper votre text</textarea>
+      <button type="button" className="btn btn-info">
+        Envoyer
+      </button>
+      <div className="footer-social-link">
+        <h3>Follow us</h3>
+        <div>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Google</li>
+            <li>linkedin</li>
+            <li>instagram</li>
+          </ul>
+        </div>
+      </div>
+      <h1>Programme</h1>
+      <hr />
+      <ul className="nav-categorie">
+        <li>Action</li>
+        <li>Animation</li>
+        <li>Aventure</li>
+        <li>Comédie</li>
+        <li>Drame</li>
+        <li>Fantastique</li>
+        <li>Horreur</li>
+        <li>Mystère</li>
+        <li>Policier</li>
+        <li>Romance</li>
+        <li>Science-Fiction</li>
+        <li>Thriller</li>
+      </ul>
+      <h1>Services</h1>
+      <hr />
+      <ul className="nav-services">
+        <li>Nos valeurs</li>
+        <li>Mention légales</li>
+        <li>Cookies</li>
+        <li>CGV</li>
+      </ul>
+    </div>
+  );
 }
 
 export default Footer;
