87678acbb770d127f2561dc8b68e3a8930d0a013
charliepian <charlie.piancatelli@gmail.com>
2022-10-26 17:39:13 +0200

    Mise en place du responsiv grand ecran

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,27 +1,33 @@
 function NavBar() {
   return (
-    <nav>
-      <div className="container-fluid ">
-        <button className="navbar-brand" type="button">
+    <nav className="navbar d-flex mb-3 bg-light">
+      <div className="me-auto p-2">
+        <button className="navbar-toggler" type="button">
           <img
             src="#"
             alt="Logo"
             width="30"
             height="24"
-            className="d-inline-block align-text-top"
+            className="navbar-brand"
           />
           StreamWood
         </button>
       </div>
-      <ul>
-        <li>Film</li>
-        <li>Mon compte</li>
-        <li>Nos valeurs</li>
+      <ul className="d-flex p-2">
+        <li className="nav-link active">Film</li>
+        <li className="nav-link active">Mon compte</li>
+        <li className="nav-link active">Nos valeurs</li>
       </ul>
-      <form>
-        <input type="text" />
-        <button type="button">BTN</button>
-      </form>
+      <div className="d-flex mb-3">
+        <form className="d-flex">
+          <input
+            className="form-control me-2"
+            placeholder="Recherche"
+            type="text"
+          />
+          <button className="navbar-toggler" type="button">btn</button>
+        </form>
+      </div>
     </nav>
   );
 }
