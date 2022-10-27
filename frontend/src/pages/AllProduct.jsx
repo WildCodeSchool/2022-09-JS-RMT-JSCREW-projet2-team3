894113127b600b337f02fed19b58431fbf98e017
arouxel17 <alexandrerouxel17@gmail.com>
2022-10-27 11:54:36 +0200

    Ajout 2eme images + background color + color

--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,10 +1,18 @@
 function AllProduct() {
   return (
     <div className="allProducts">
-      <h2>Tous les films</h2>
-      <h3>Catégories</h3>
+      <div className="d-flex align-items-start word-spacing: 1rem">
+        <h2>Tous les films</h2>
+        <h3>Catégories</h3>
+      </div>
+      
       <img
-        className="col-md-1 img-fluid"
+        className="col-5 img-fluid"
+        src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
+        alt="1er film action"
+      />
+      <img
+        className="col-5 img-fluid"
         src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
         alt="1er film action"
       />
