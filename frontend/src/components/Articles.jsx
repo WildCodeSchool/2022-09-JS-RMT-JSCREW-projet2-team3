fe298ad0360f7a8a292d6d9d949b1fbb8e00dbf1
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-10-28 09:54:46 +0200

    mise en place du composant article et css responsive

---
+++ frontend/src/components/Articles.jsx
@@ -0,0 +1,21 @@
+function Articles() {
+  return (
+    <div className="my-4 mx-2">
+    <article className="row">
+      <div className="col-5 col-md-8">
+       <img
+        src="https://www.mdr-services.com/wp-content/uploads/2022/06/serveur-vert-2-e1654951207296.png"
+        alt="serveur"
+        className="w-100"
+       />
+       </div>
+      <p className="col-7 col-md-4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        laboriosam nulla tempora consectetur officia facere, beatae.
+      </p>
+    </article>
+    </div>
+  )
+}
+
+export default Articles
--- frontend/src/pages/OurValues.jsx
+++ frontend/src/pages/OurValues.jsx
@@ -1,6 +1,8 @@
+import Articles from "@components/Articles";
+
 function OurValues() {
   return (
-    <div>
+    <div className="container-fluid">
       <header>
         <h1>our values</h1>
         <p>
@@ -15,37 +17,30 @@ function OurValues() {
       <img
         src="https://www.jardinautes.com/wp-content/uploads/2021/10/quel-arbre-fruitier-planter-dans-son-jardin.jpg"
         alt="arbre plante"
+        className="img-fluid"
       />
-      <div className="container">
-        <div className="row">
-          <article className="col-md-4 d-flex">
-            <img
-              src="https://tophebergeur.b-cdn.net/images/varia/meilleur-hebergeur-vert-ecologique-ecoresponsable-min.jpg"
-              alt="serveur"
-              className="w-75"
-            />
-            <p className="w-25">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-              laboriosam nulla tempora consectetur officia facere, beatae.
-            </p>
-          </article>
-          <article className="col-md-4 d-flex">
+      <div className="container-fluid">
+        <Articles />
+          <div className="row">
+           <article className="">
             <img
               src="https://www.neozone.org/blog/wp-content/uploads/schema-and-structured-data-for-wp/panneaux-solaires-plus-efficaces-moins-cher-durables-nouveau-materiau-001-1200x675.jpg"
               alt="panneaux electrique"
             />
-            <p className="w-25">
+            <p className="">
               Nos panneaux solaires produisent le double de l’énergie nécessaire
               pour alimenter vos recherches. Cette ressource renouvelable permet
               de réduire l’utilisation de combustibles fossiles.
             </p>
           </article>
-          <article className="col-md-4 d-flex">
+          </div>
+          <div className="row">
+          <article className="">
             <img
               src="https://www.neumediatech.com/wp-content/uploads/2018/02/ecologie-informatique-web-732x380.jpg"
               alt="devellopement vert"
             />
-            <p className="w-25">
+            <p className="">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ea
               consectetur veniam unde cumque reiciendis optio
             </p>
