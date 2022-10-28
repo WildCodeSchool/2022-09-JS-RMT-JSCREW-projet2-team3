6bb11fe8d557eefd6078847d9887004d455a97e3
arouxel17 <alexandrerouxel17@gmail.com>
2022-10-28 10:23:02 +0200

    espace entre les affiches

--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,19 +1,28 @@
 function AllProduct() {
   return (
-    <div className="allProducts">
-      <div className="d-flex align-items-start word-spacing: 1rem">
+    <div className="container-fluid">
+      <div className="d-flex justify-content-start p-2">
         <h2>Tous les films</h2>
         <h3>Catégories</h3>
       </div>
-      
       <img
-        className="col-5 img-fluid"
+        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
         src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
         alt="1er film action"
       />
       <img
-        className="col-5 img-fluid"
-        src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
+        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
+        src="https://image.tmdb.org/t/p/w500/fenNPxVF5ERy0CSyVruuEg959Hg.jpg"
+        alt="1er film action"
+      />
+       <img
+        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
+        src="https://image.tmdb.org/t/p/w500/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg"
+        alt="1er film action"
+      />
+        <img
+        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
+        src="https://image.tmdb.org/t/p/w500/spCAxD99U1A6jsiePFoqdEcY0dG.jpg"
         alt="1er film action"
       />
     </div>
