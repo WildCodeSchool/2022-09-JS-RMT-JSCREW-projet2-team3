652e665db45fb9cdd1c6d28ded341be1103fcc04
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-10-28 10:23:24 +0200

    modification article pour possitionement gauche avec props

--- frontend/src/components/Articles.jsx
+++ frontend/src/components/Articles.jsx
@@ -9,7 +9,12 @@ function Articles({ text, image, alt, position }) {
           <p className="col-7 col-md-4">{text}</p>
         </article>
       ) : (
-        ""
+        <article className="row">
+          <p className="col-7 col-md-4">{text}</p>
+          <div className="col-5 col-md-8">
+            <img src={image} alt={alt} className="w-100" />
+          </div>
+        </article>
       )}
     </div>
   );
