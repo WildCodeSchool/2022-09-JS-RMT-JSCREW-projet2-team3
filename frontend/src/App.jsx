52be3bbe92963728af62b8925e9a3b4828e00e81
charliepian <charlie.piancatelli@gmail.com>
2022-10-30 09:07:01 +0100

    Ajout de la page account sur App.jsx et ajout des liens entre les pages avec state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Home from "@pages/Home";
 import AllProduct from "@pages/AllProduct";
 import OneProduct from "@pages/OneProduct";
@@ -5,22 +7,26 @@ import NavBar from "@components/NavBar";
 import Footer from "@components/Footer";
 import Compteur from "@components/Compteur";
 import Subscribe from "@pages/Subscribe";
+import Account from "@pages/Account";
 
 import filmsData from "@services/films";
 
 import "./App.css";
 
 function App() {
+  const [page, setPage] = useState("Home");
+
   return (
     <div className="App">
-      <NavBar />
-      <Home filmsData={filmsData} />
-      <AllProduct filmsData={filmsData} />
-      <OneProduct filmsData={filmsData} />
+      <NavBar setPage={setPage} />
+      {page === "Home" ? <Home filmsData={filmsData} /> : ""}
+      {page === "AllProduct" ? <AllProduct filmsData={filmsData} /> : ""}
+      {page === "OneProduct" ? <OneProduct filmsData={filmsData} /> : ""}
+      {page === "Account" ? <Account filmsData={filmsData} /> : ""}
       <Compteur />
       <Subscribe />
       <Footer />
-   </div>
+    </div>
   );
 }
 
