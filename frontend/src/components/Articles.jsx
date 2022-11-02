e74052ad4d938820d4c49c22f1c6cb35503b674a
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-11-02 21:17:47 +0100

    ajout responsive sur le texte article

--- frontend/src/components/Articles.jsx
+++ frontend/src/components/Articles.jsx
@@ -1,21 +1,26 @@
 function Articles({ text, image, alt, position }) {
   return (
-    <div className="my-4 mx-2">
-      {position === "right" ? (
-        <article className="row">
-          <div className="col-5 col-md-8">
-            <img src={image} alt={alt} className="w-100" />
-          </div>
-          <p className="col-7 col-md-4">{text}</p>
-        </article>
-      ) : (
-        <article className="row">
-          <p className="col-7 col-md-4">{text}</p>
-          <div className="col-5 col-md-8">
-            <img src={image} alt={alt} className="w-100" />
-          </div>
-        </article>
-      )}
+    <div className="col-md-4 col-12">
+      <div className="my-4 mx-2">
+        {position === "right" ? (
+          <article className="row align-items-center">
+            <div className="col-5 col-md-6">
+              <img src={image} alt={alt} className="w-100" />
+            </div>
+            <p className="col-7 col-md-6">{text}</p>
+          </article>
+        ) : (
+          <article className="row align-items-center">
+            <div className="col-5 col-md-6 	d-none d-sm-block">
+              <img src={image} alt={alt} className="w-100" />
+            </div>
+            <p className="col-7 col-md-6">{text}</p>
+            <div className="col-5 col-md-6 d-block d-sm-none">
+              <img src={image} alt={alt} className="w-100" />
+            </div>
+          </article>
+        )}
+      </div>
     </div>
   );
 }
--- frontend/src/pages/OurValues.jsx
+++ frontend/src/pages/OurValues.jsx
@@ -6,12 +6,13 @@ function OurValues() {
       <header>
         <h1>Our Values</h1>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure ea aut
-          quod beatae, vero molestiae sit dolore quidem neque tempore, quis eum
-          qui quaerat distinctio odio fugiat in fuga similique mollitia
-          explicabo inventore adipisci! Optio, iure harum cumque reiciendis
-          natus, nesciunt quaerat quas obcaecati veniam magnam animi itaque
-          velit rerum.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+          necessitatibus deleniti officia eveniet facere, commodi maxime, quae
+          esse animi sit corporis ex saepe laboriosam omnis sapiente ratione ea
+          ut reprehenderit! Nemo quaerat odio esse reprehenderit, voluptates
+          tempora sint tempore illo suscipit? Tempora, aliquam sequi assumenda
+          maiores ab qui magnam distinctio odio quidem quae, reprehenderit dicta
+          quaerat perspiciatis ut doloremque voluptates?
         </p>
       </header>
       <img
@@ -21,28 +22,28 @@ function OurValues() {
       />
       <div className="container">
         <div className="row">
-            <Articles
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-          laboriosam nulla tempora consectetur officia facere, beatae."
-          image="./src/assets/image/serveur.png"
-          alt="serveur"
-          position="right"
-            />
-            <Articles
-              text="Nos panneaux solaires produisent le double de l’énergie nécessaire
+          <Articles
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          laboriosam nulla tempora consectetur officia facere "
+            image="./src/assets/image/serveur.png"
+            alt="serveur"
+            position="right"
+          />
+          <Articles
+            text="Nos panneaux solaires produisent le double de l’énergie nécessaire
          pour alimenter vos recherches. Cette ressource renouvelable permet
          de réduire l’utilisation de combustibles fossiles."
-          image="./src/assets/image/panneauvert.png"
-          alt="panneaux electrique"
-          position="left"
-            />
-            <Articles
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ea
-        consectetur veniam unde cumque reiciendis optio"
-          image="./src/assets/image/developmentweb.png"
-          alt="devellopement vert"
-          position="right"
-            />
+            image="./src/assets/image/panneauvert.png"
+            alt="panneaux electrique"
+            position="left"
+          />
+          <Articles
+            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ea
+        consectetur veniam unde cumque reiciendis optioLorem ipsum dolor sit amet consectetur"
+            image="./src/assets/image/developmentweb.png"
+            alt="devellopement vert"
+            position="right"
+          />
         </div>
       </div>
     </div>
