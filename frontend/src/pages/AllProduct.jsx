2dbd5e8d6f7b04668bb4cfa1b5a014715a31d0d3
arouxel17 <alexandrerouxel17@gmail.com>
2022-11-03 10:18:44 +0100

    affichage de tous les films ok

--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,30 +1,16 @@
+import PosterMovie from "@components/PosterMovie";
+import filmsData from "@services/films";
+
 function AllProduct() {
   return (
-    <div className="container-fluid">
-      <div className="d-flex justify-content-start p-2">
-        <h2>Tous les films</h2>
+    <div className="container">
+      <div className="d-flex text-align-center">
+        <h2 className="me-3">Tous les films</h2>
         <h3>Catégories</h3>
       </div>
-      <img
-        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
-        src="https://image.tmdb.org/t/p/w500/7ze7YNmUaX81ufctGqt0AgHxRtL.jpg"
-        alt="1er film action"
-      />
-      <img
-        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
-        src="https://image.tmdb.org/t/p/w500/fenNPxVF5ERy0CSyVruuEg959Hg.jpg"
-        alt="1er film action"
-      />
-       <img
-        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
-        src="https://image.tmdb.org/t/p/w500/tVxDe01Zy3kZqaZRNiXFGDICdZk.jpg"
-        alt="1er film action"
-      />
-        <img
-        className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
-        src="https://image.tmdb.org/t/p/w500/spCAxD99U1A6jsiePFoqdEcY0dG.jpg"
-        alt="1er film action"
-      />
+      {filmsData && filmsData.map( (movie) => 
+        <PosterMovie movie={movie}/>
+      )};
     </div>
   );
 }
