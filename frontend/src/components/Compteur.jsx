83623243fd0efe064a93bfcfd87217d747d24afc
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-11-04 10:41:05 +0100

    mise en place du html et js pour le compteur

--- frontend/src/components/Compteur.jsx
+++ frontend/src/components/Compteur.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+
+let timer;
 function Compteur() {
-  return <div>Compteur</div>;
+  const [counterState, setCounter] = React.useState(8796);
+  React.useEffect(() => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+      setCounter((prev) => prev + 1);
+    }, 670);
+
+    return () => clearInterval(timer);
+  }, [counterState]);
+
+  return <div>{counterState}</div>;
 }
 
 export default Compteur;
