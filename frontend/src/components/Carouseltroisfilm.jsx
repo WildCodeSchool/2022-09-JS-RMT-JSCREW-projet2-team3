9d332d4981ade722a5f502335349f8e7a4afc82e
serpentaire <glemoine@hotmail.fr>
2022-11-07 22:34:53 +0100

    mise en place du carousel 3 images sans ouverture de la page du film

--- frontend/src/components/Carouseltroisfilm.jsx
+++ frontend/src/components/Carouseltroisfilm.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import filmsData from "@services/films";
+import OneProduct from "@pages/OneProduct";
 
 import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
 
 function Carouseltroisimage() {
-  const src1 =
-    filmsData[Math.floor(Math.random() * filmsData.length)].backdrop_path;
-  const src2 =
-    filmsData[Math.floor(Math.random() * filmsData.length)].backdrop_path;
-  const src3 =
-    filmsData[Math.floor(Math.random() * filmsData.length)].backdrop_path;
+  const filmId1 = Math.floor(Math.random() * filmsData.length);
+  const filmId2 = Math.floor(Math.random() * filmsData.length);
+  const filmId3 = Math.floor(Math.random() * filmsData.length);
+
+  const openPageOneProduct = (filmData) => {
+    <OneProduct filmData={filmData} />;
+  };
+
   return (
     <div className="carousel w-100 pt-4">
       <div className="container-fluid col-11 col-lg-8">
@@ -19,20 +22,23 @@ function Carouseltroisimage() {
           <MDBCarouselItem
             className="w-100 d-block rounded-9"
             itemId={1}
-            src={src1}
+            src={filmsData[filmId1].backdrop_path}
             alt="film1"
+            onClick={() => openPageOneProduct(filmsData[filmId1])}
           />
           <MDBCarouselItem
             className="w-100 d-block rounded-9"
             itemId={2}
-            src={src2}
+            src={filmsData[filmId2].backdrop_path}
             alt="film2"
+            onClick={() => openPageOneProduct(filmsData[filmId2])}
           />
           <MDBCarouselItem
             className="w-100 d-block rounded-9"
             itemId={3}
-            src={src3}
+            src={filmsData[filmId3].backdrop_path}
             alt="film3"
+            onClick={() => openPageOneProduct(filmsData[filmId3])}
           />
         </MDBCarousel>
       </div>
