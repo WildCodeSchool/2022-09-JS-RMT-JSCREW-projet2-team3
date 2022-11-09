e6725f211f20c5a836de85e3ba5bcbe2c8868bc1
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-11-09 12:21:53 +0100

    ajout text anglais , et responsive ourvalor

--- frontend/src/components/Compteur.jsx
+++ frontend/src/components/Compteur.jsx
@@ -2,17 +2,38 @@ import React from "react";
 
 let timer;
 function Compteur() {
-  const [counterState, setCounter] = React.useState(8796);
+  const [counterState, setCounter] = React.useState(81796);
   React.useEffect(() => {
     clearInterval(timer);
     timer = setInterval(() => {
       setCounter((prev) => prev + 1);
-    }, 670);
+    }, 800);
 
     return () => clearInterval(timer);
   }, [counterState]);
 
-  return <div>{counterState}</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="">
+          <button
+            type="button"
+            className="btn btn-compteur w-25 rounded-5 text-white display-1"
+          >
+            <img
+              src="./src/assets/image/plantd'arbre.png"
+              alt="icone compteur"
+              height={40}
+            />
+            <div className="display-4">{counterState}</div>
+            <p>
+              <strong>Learn more 👈</strong>
+            </p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Compteur;
