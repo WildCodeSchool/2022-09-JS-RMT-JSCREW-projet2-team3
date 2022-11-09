1d45013afd264fa622210ecb9a75c5e308047ba4
charliepian <charlie.piancatelli@gmail.com>
2022-11-09 11:59:45 +0100

    Modificaiton des page avec Path et ajout de Onclick avec HandlePage

--- frontend/src/components/PosterMovie.jsx
+++ frontend/src/components/PosterMovie.jsx
@@ -1,10 +1,15 @@
 function PosterMovie({ movie }) {
   return (
-    <img
-      className="col-5 col-sm-3 col-md-2 col-lg-1 img-fluid p-2"
-      src={movie.poster_path}
-      alt={movie.title}
-    />
+    <button
+      type="button"
+      className="navbar-toggler hover-btn col-5 col-sm-3 col-md-3 col-lg-2"
+    >
+      <img
+        className="img-fluid p-2"
+        src={movie.poster_path}
+        alt={movie.title}
+      />
+    </button>
   );
 }
 
--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,14 +1,20 @@
 import PosterMovie from "@components/PosterMovie";
 import filmsData from "@services/films";
 
-function AllProduct() {
+function AllProduct({ setPage }) {
   return (
     <div className="container">
       <div className="d-flex text-align-center">
         <h2 className="me-3">Tous les films</h2>
         <h3>Catégories</h3>
       </div>
-      {filmsData && filmsData.map((movie) => <PosterMovie movie={movie} />)};
+      {filmsData &&
+        filmsData.map((movie, index) => (
+          <PosterMovie
+            movie={movie}
+            handlePage={() => setPage({ path: "OneProduct", id: index })}
+          />
+        ))}
     </div>
   );
 }
