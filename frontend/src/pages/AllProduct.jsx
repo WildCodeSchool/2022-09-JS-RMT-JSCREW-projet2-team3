7ded8bcada5dad15ebb14822ef8197220a41938f
arouxel17 <alexandrerouxel17@gmail.com>
2022-11-09 10:29:17 +0100

    Add liaison back/front sur la allproduct

--- frontend/src/pages/AllProduct.jsx
+++ frontend/src/pages/AllProduct.jsx
@@ -1,14 +1,23 @@
 import PosterMovie from "@components/PosterMovie";
-import filmsData from "@services/films";
+import {useEffect, useState} from 'react';
 
 function AllProduct() {
+  const [filmsData, setFilmsData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/movies')
+    .then((response) => response.json())
+    .then((data) => setFilmsData(data)) 
+    .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className="container">
       <div className="d-flex text-align-center">
         <h2 className="me-3">Tous les films</h2>
         <h3>Catégories</h3>
       </div>
-      {filmsData && filmsData.map((movie) => <PosterMovie movie={movie} />)};
+      {filmsData.map((movie) => <PosterMovie movie={movie} />)};
     </div>
   );
 }
