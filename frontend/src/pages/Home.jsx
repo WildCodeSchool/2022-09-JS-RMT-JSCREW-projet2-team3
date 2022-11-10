49441362c5e84d9b14d1f991c61fd86d9b47474a
serpentaire <glemoine@hotmail.fr>
2022-11-10 21:49:14 +0100

    mise en place clic sur carrousel 3 films vers page OneProduct

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import CarrouselThreeFilms from "../components/Carrousel/Threefilm";
 
-export default function Home() {
+export default function Home({setPage}) {
   return (
     <div>
-      <CarrouselThreeFilms />
+      <CarrouselThreeFilms setPage={setPage}/>
       <h1>HOME</h1>
     </div>
   );
