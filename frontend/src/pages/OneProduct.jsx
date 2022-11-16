13a9785438868d70e9a58ae0ca512d641559490f
Amina HAKIMI <amina.hakimi86@gmail.com>
2022-11-16 12:04:27 +0100

    Hotfix OneProduct

--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 
-import "./OneProduct.css";
-
 function OneProduct({ id }) {
   const [movie, setMovie] = useState();
 
@@ -13,49 +11,92 @@ function OneProduct({ id }) {
   }, []);
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid ">
       {movie && (
-        <div className="filmCard">
+        <div className="img-fluid">
           <img
-            className="img-fluid rounded mx-auto d-block movie-image"
+            className="card-img-fluid-oneproduct text-center w-100 mx-auto opacity-80"
             src={movie.backdrop_path}
             alt={movie.original_title}
           />
+          <div className="card-img-overlay text-start mt-5 ms-2 d-none d-md-block d-lg-block">
+            <h2 className="card-title-oneproduct mt-5 text-white mb-5 title-oneproduct">
+              {movie.title}
+            </h2>
+            <div className="container-oneproduct">
+              <div className="d-flex justify-content-start">
+                <span className="card-texte text-start text-white me-4 fs-5">
+                  {movie.genre_ids}
+                </span>
+                <span className="card-texte text-white fs-5 mb-0 me-2">
+                  {movie.release_date}
+                </span>
+              </div>
+            </div>
+            <div className="row">
+              <p className="card-texte text-white text-break fs-5 mt-5">
+                {movie.overview}
+              </p>
+            </div>
+            <div className="container-btn">
+              <button
+                type="button"
+                className="btn btn-md ms-3 me-2 mt-5 btn-css btn-success text-white bouton-watch"
+              >
+                <i className="fa-solid fa-play" />
+              </button>
+              <span className="btn btn-md me-2 mt-5 btn-css btn-success">
+                <b>Popularity : {movie.popularity}</b>
+              </span>
+              <button
+                type="button"
+                className="btn btn-md me-2 mt-5 btn-css btn-sucess btn-success"
+              >
+                <b>
+                  {movie.vote_average}
+                  <small> (vote)</small>
+                </b>
+              </button>
+            </div>
+          </div>
+
           <div className="container-btn">
             <button
               type="button"
-              className="btn text-white btn-md ms-3 me-2 mt-5 btn-css"
+              className="btn btn-md ms-3 me-2 mt-5 btn-css btn-success d-md-none d-lg-none"
             >
-              <b>Voir</b>
+              <i className="fa-solid fa-play" />
             </button>
+            <span className="btn btn-md me-2 mt-5 btn-css btn-success d-md-none d-lg-none">
+              <b>Popularity :{movie.popularity}</b>
+            </span>
             <button
               type="button"
-              className="btn text-white btn-md me-2 mt-5 btn-css"
+              className="btn btn-md me-2 mt-5 btn-css btn-sucess btn-success d-md-none d-lg-none"
             >
-              <b>Popularité</b>
-            </button>
-            <button
-              type="button"
-              className="btn text-white btn-md me-2 mt-5 btn-css"
-            >
-              <b>Vote : {movie.vote_average}</b>
+              <b>
+                {movie.vote_average}
+                <small>(vote)</small>
+              </b>
             </button>
           </div>
           <div className="content">
-            <h3 className="main-title text-start font-weight-bold ms-3 mt-5">
+            <h3 className="main-title text-start font-weight-bold ms-3 mt-5 d-md-none d-lg-none">
               {movie.original_title}
             </h3>
             <div className="container ms-0">
               <div className="d-flex justify-content-start">
-                <span className="text-start ms-2 me-4">
-                  Genre : {movie.genre_ids}
+                <span className="text-start ms-2 me-4 d-md-none d-lg-none">
+                  {movie.genre_ids}
                 </span>
-                <span className="text-start me-2">
-                  Sortie : {movie.release_date}
+                <span className="text-start me-2 d-md-none d-lg-none">
+                  {movie.release_date}
                 </span>
               </div>
             </div>
-            <p className="text-start ms-3 mt-5">{movie.overview}</p>
+            <p className="text-start ms-3 mt-3 d-md-none d-lg-none">
+              {movie.overview}
+            </p>
           </div>
           <h5 className="text-start ms-3 mb-1 mt-3">Suggestions</h5>
         </div>
