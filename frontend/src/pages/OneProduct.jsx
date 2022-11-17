4b820232db828aa7d08991e584d0ce90e5f3bcc4
arouxel17 <alexandrerouxel17@gmail.com>
2022-11-17 10:21:52 +0100

    hotfix sur le nombre affiches par colonne + suppression de la page oneproduct.css qui nous embete

--- frontend/src/pages/OneProduct.jsx
+++ frontend/src/pages/OneProduct.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-import "./OneProduct.css";
-
 function OneProduct() {
   const [movie, setMovie] = useState();
   const { id } = useParams();
