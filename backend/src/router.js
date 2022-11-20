2b8328335c5951e8fc823d2ebf9a26249bae71d1
serpentaire <glemoine@hotmail.fr>
2022-11-20 18:53:57 +0100

    Connection de threefilm à la db

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const { connect } = require("./db");
 const router = express.Router();
 
 const movies = require("./data/movies");
@@ -17,6 +17,18 @@ router.get("/Films/:id", (req, res) => {
   } else {
     res.status(404).send("Not Found");
   }
+});
+
+router.get("/Threefilm", (red,res) => {
+  connect
+    .query("SELECT * FROM movies ORDER BY RAND() LIMIT 3")
+    .then(([response]) => {
+       res.status(200).send(response);
+       })
+       .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
--- frontend/src/components/Carrousel/Threefilm.jsx
+++ frontend/src/components/Carrousel/Threefilm.jsx
@@ -1,103 +1,125 @@
-import React from "react";
-import filmsData from "@services/films";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function CarrouselThreeFilms() {
-  const getRandom = () => Math.floor(Math.random() * filmsData.length);
+  const [threeFilmId, setThreeFilmId] = useState([]);
 
-  const filmId1 = getRandom();
-  const filmId2 = getRandom();
-  const filmId3 = getRandom();
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/Threefilm`)
+      .then((response) => response.json())
+      .then((data) => setThreeFilmId(data))
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  let filmId1 = 1;
+  let filmId2 = 2;
+  let filmId3 = 3;
+  let filmsrc1 = "";
+  let filmsrc2 = "";
+  let filmsrc3 = "";
+
+  if (threeFilmId[0]) {
+    filmId1 = parseInt(threeFilmId[0].id, 10);
+    filmId2 = parseInt(threeFilmId[1].id, 10);
+    filmId3 = parseInt(threeFilmId[2].id, 10);
+    filmsrc1 = threeFilmId[0].backdrop_path;
+    filmsrc2 = threeFilmId[1].backdrop_path;
+    filmsrc3 = threeFilmId[2].backdrop_path;
+  }
 
   return (
-    <div className="carousel w-100 pt-4">
-      <div className="container-fluid col-11 col-lg-8">
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            />
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            />
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            />
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel3film carousel-item active">
-              <Link to={`/AllProduct/${filmId1 + 1}`}>
-                <img
-                  src={filmsData[filmId1].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={filmId1}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-            <div className="carousel3film carousel-item">
-              <Link to={`/AllProduct/${filmId2 + 1}`}>
-                <img
-                  src={filmsData[filmId2].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={filmId2}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-            <div className="carousel3film carousel-item">
-              <Link to={`/AllProduct/${filmId3 + 1}`}>
-                <img
-                  src={filmsData[filmId3].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={filmId3}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
+    threeFilmId && (
+      <div className="carousel w-100 pt-4">
+        <div className="container-fluid col-11 col-lg-8">
+          <div
+            id="carouselExampleIndicators"
+            className="carousel slide"
+            data-bs-ride="carousel"
           >
-            <span className="carousel-control-prev-icon" aria-hidden="true" />
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span className="carousel-control-next-icon" aria-hidden="true" />
-            <span className="visually-hidden">Next</span>
-          </button>
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              />
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              />
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              />
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel3film carousel-item active">
+                <Link to={`/AllProduct/${filmId1}`}>
+                  <img
+                    src={filmsrc1}
+                    className="imagecarouselligne card-img-top"
+                    alt={filmId1}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p>TOP 3 DE LA SEMAINE</p>
+                </div>
+              </div>
+              <div className="carousel3film carousel-item">
+                <Link to={`/AllProduct/${filmId2}`}>
+                  <img
+                    src={filmsrc2}
+                    className="imagecarouselligne card-img-top"
+                    alt={filmId2}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p>TOP 3 DE LA SEMAINE</p>
+                </div>
+              </div>
+              <div className="carousel3film carousel-item">
+                <Link to={`/AllProduct/${filmId3}`}>
+                  <img
+                    src={filmsrc3}
+                    className="imagecarouselligne card-img-top"
+                    alt={filmId3}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p>TOP 3 DE LA SEMAINE</p>
+                </div>
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
