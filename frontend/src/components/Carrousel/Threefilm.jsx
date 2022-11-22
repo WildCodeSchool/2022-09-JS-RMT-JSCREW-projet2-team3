aa4e9cce7a215cb5561bb0ef8ee999c84670adbf
jordanvax <jordan.vaxelaire@hotmail.fr>
2022-11-22 15:22:01 +0100

    traduction text et modif css caroussel trois films

--- frontend/src/components/Carrousel/Threefilm.jsx
+++ frontend/src/components/Carrousel/Threefilm.jsx
@@ -15,93 +15,100 @@ function CarrouselThreeFilms() {
 
   return (
     <div className="carousel w-100 pt-4">
-      {threeFilmId && <div className="container-fluid col-11 col-lg-8">
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide"
-          data-bs-ride="carousel"
-        >
-          <div className="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="0"
-              className="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            />
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            />
-            <button
-              type="button"
-              data-bs-target="#carouselExampleIndicators"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            />
-          </div>
-          <div className="carousel-inner">
-            <div className="carousel3film carousel-item active">
-              <Link to={`/AllProduct/${threeFilmId[0].id}`}>
-                <img
-                  src={threeFilmId[0].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={threeFilmId[0].id}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-            <div className="carousel3film carousel-item">
-              <Link to={`/AllProduct/${threeFilmId[1].id}`}>
-                <img
-                  src={threeFilmId[1].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={threeFilmId[1].id}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-            <div className="carousel3film carousel-item">
-              <Link to={`/AllProduct/${threeFilmId[2].id}`}>
-                <img
-                  src={threeFilmId[2].backdrop_path}
-                  className="imagecarouselligne card-img-top"
-                  alt={threeFilmId[2].id}
-                />
-              </Link>
-              <div className="carousel-caption d-block col-4">
-                <p>TOP 3 DE LA SEMAINE</p>
-              </div>
-            </div>
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="prev"
+      {threeFilmId && (
+        <div className="container-fluid col-11 col-lg-8">
+          <div
+            id="carouselExampleIndicators"
+            className="carousel slide"
+            data-bs-ride="carousel"
           >
-            <span className="carousel-control-prev-icon" aria-hidden="true" />
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide="next"
-          >
-            <span className="carousel-control-next-icon" aria-hidden="true" />
-            <span className="visually-hidden">Next</span>
-          </button>
+            <div className="carousel-indicators">
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="0"
+                className="active"
+                aria-current="true"
+                aria-label="Slide 1"
+              />
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="1"
+                aria-label="Slide 2"
+              />
+              <button
+                type="button"
+                data-bs-target="#carouselExampleIndicators"
+                data-bs-slide-to="2"
+                aria-label="Slide 3"
+              />
+            </div>
+            <div className="carousel-inner">
+              <div className="carousel3film carousel-item active">
+                <Link to={`/AllProduct/${threeFilmId[0].id}`}>
+                  <img
+                    src={threeFilmId[0].backdrop_path}
+                    className="imagecarouselligne card-img-top"
+                    alt={threeFilmId[0].id}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p className="display-4 pcarouselltopthree">
+                    TOP 3 OF THE WEEK
+                  </p>
+                </div>
+              </div>
+              <div className="carousel3film carousel-item">
+                <Link to={`/AllProduct/${threeFilmId[1].id}`}>
+                  <img
+                    src={threeFilmId[1].backdrop_path}
+                    className="imagecarouselligne card-img-top"
+                    alt={threeFilmId[1].id}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p className="display-4 pcarouselltopthree">
+                    TOP 3 OF THE WEEK
+                  </p>
+                </div>
+              </div>
+              <div className="carousel3film carousel-item">
+                <Link to={`/AllProduct/${threeFilmId[2].id}`}>
+                  <img
+                    src={threeFilmId[2].backdrop_path}
+                    className="imagecarouselligne card-img-top"
+                    alt={threeFilmId[2].id}
+                  />
+                </Link>
+                <div className="carousel-caption d-block col-4">
+                  <p className="display-4 pcarouselltopthree">
+                    TOP 3 OF THE WEEK
+                  </p>
+                </div>
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="prev"
+            >
+              <span className="carousel-control-prev-icon" aria-hidden="true" />
+              <span className="visually-hidden">Previous</span>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide="next"
+            >
+              <span className="carousel-control-next-icon" aria-hidden="true" />
+              <span className="visually-hidden">Next</span>
+            </button>
+          </div>
         </div>
-      </div>
-      }
+      )}
     </div>
   );
 }
